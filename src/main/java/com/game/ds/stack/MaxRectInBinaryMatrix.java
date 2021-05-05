4dc9676c18684b6c966c973f18b176d1f6a8f272
bittu <akshaychincholkar@gmail.com>
2021-05-05 15:29:27 +0530

    Maximum Area of rectangle in binary subarray

--- src/main/java/com/game/ds/stack/MaxRectInBinaryMatrix.java
+++ src/main/java/com/game/ds/stack/MaxRectInBinaryMatrix.java
@@ -12,6 +12,7 @@ public class MaxRectInBinaryMatrix {
 
     public static int getMaxSizeRectangle(int[][] a) {
         int[] building = new int[a[0].length];
+        int maxArea = 0;
         for(int j = 0;j<a.length;j++){
             for(int i=0;i<a[0].length;i++){
                 System.out.print(a[j][i]+"\t");
@@ -19,9 +20,11 @@ public class MaxRectInBinaryMatrix {
                 else building[i] += a[j][i];
             }
 //            for(int element: building) System.out.print(element+"\t");
-            System.out.println("----------Maximum area:"+MaximumAreaInHistogram.getLargestRectagleArea(building));
+            int area = MaximumAreaInHistogram.getLargestRectagleArea(building);
+            System.out.println("----------Maximum area:"+area);
+            if(maxArea<area)maxArea = area;
             System.out.println();
         }
-    return 0;
+    return maxArea;
     }
 }
