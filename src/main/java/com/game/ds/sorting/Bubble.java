e44ce8d6a4dc3c115de58b786b1a684f7ac1b421
bittu <akshaychincholkar@gmail.com>
2021-05-06 21:24:44 +0530

    Recursion

--- src/main/java/com/game/ds/sorting/Bubble.java
+++ src/main/java/com/game/ds/sorting/Bubble.java
@@ -2,6 +2,7 @@ package com.game.ds.sorting;
 
 public class Bubble {
     static void sort(int[] a){
+        int count =0;
         for(int i=0;i<a.length;i++)
             for(int j=0;j<a.length-i-1;j++){
                 if(a[j]>a[j+1]){
@@ -10,10 +11,12 @@ public class Bubble {
                     a[j+1]=t;
                 }
             }
+        System.out.println(count);
     }
     public static void main(String args[]){
         System.out.println("Bubble sort program");
-        int[] a ={5,4,3,2,1};
+//        int[] a ={5,4,3,2,1};
+        int[] a ={1,2,3,4,5};
         for(int i:a) System.out.println(i);
         sort(a);
         System.out.println("After sorting");
