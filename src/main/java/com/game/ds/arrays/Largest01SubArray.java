3b673d82ac8ee484f07aa61b40f6eee4a8b15fa8
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-04-14 02:55:31 +0530

    Updates to the implementations

--- src/main/java/com/game/ds/arrays/Largest01SubArray.java
+++ src/main/java/com/game/ds/arrays/Largest01SubArray.java
@@ -1,10 +1,28 @@
 package com.game.ds.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Largest01SubArray {
     public static void main(String[] args) {
-        int[]  a = {1,1,0,0,1,1,1,1,1,0,0,0,0,0};
+        int[]  a = {1,1,0,0,1,1,1,1,1,0,0,0,0,1};
 //        int[] a = {1, 0, 1, 1, 1, 0, 0};
-        printLargestSubarray(a);
+//        printLargestSubarray(a);
+        System.out.println(findMaxLength(a));
+    }
+    public static int findMaxLength(int[] nums) {
+        Map< Integer, Integer > map = new HashMap < > ();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+        return maxlen;
     }
 
     private static void printLargestSubarray(int[] a) {
@@ -42,4 +60,59 @@ public class Largest01SubArray {
         }
         System.out.println("Subarray is from index: "+l_ptr+ " to "+r_ptr);
     }
+    public static int maxLen(int arr[], int n)
+    {
+        // Creates an empty hashMap hM
+
+        HashMap<Integer, Integer> hM
+                = new HashMap<Integer, Integer>();
+
+        // Initialize sum of elements
+        int sum = 0;
+
+        // Initialize result
+        int max_len = 0;
+        int ending_index = -1;
+        int start_index = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (arr[i] == 0) ? -1 : 1;
+        }
+
+        // Traverse through the given array
+
+        for (int i = 0; i < n; i++) {
+            // Add current element to sum
+
+            sum += arr[i];
+
+            // To handle sum=0 at last index
+
+            if (sum == 0) {
+                max_len = i + 1;
+                ending_index = i;
+            }
+
+            // If this sum is seen before,
+            // then update max_len if required
+            if (hM.containsKey(sum)) {
+                if (max_len < i - hM.get(sum)) {
+                    max_len = i - hM.get(sum);
+                    ending_index = i;
+                }
+            }
+            else // Else put this sum in hash table
+                hM.put(sum, i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (arr[i] == -1) ? 0 : 1;
+        }
+
+        int end = ending_index - max_len + 1;
+        System.out.println(end + " to " + ending_index);
+
+        return max_len;
+    }
+
 }
