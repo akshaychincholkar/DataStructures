cb436171b54ed4551eba437ba65946b5bc435a09
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-04-19 00:35:46 +0530

    Pivot Index problem

---
+++ src/main/java/com/game/ds/arrays/PivotIndex.java
@@ -0,0 +1,20 @@
+package com.game.ds.arrays;
+
+public class PivotIndex {
+    public static void main(String[] args) {
+        int[] nums = {1,7,3,6,5,6};
+        System.out.println("Index: "+getPivotIndex(nums));
+    }
+
+    private static int getPivotIndex(int[] nums) {
+        int total_count =0;
+        for(int num: nums)total_count+=num;
+        if(0 == total_count - nums[0])return 0;
+        int left_sum = 0;
+        for(int i=1;i<nums.length;i++){
+            left_sum  += nums[i-1];
+            if(2*left_sum == total_count - nums[i]) return i;
+        }
+        return -1;
+    }
+}
