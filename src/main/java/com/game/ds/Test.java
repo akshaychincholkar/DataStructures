e880385f156f39af4e663b837f7831988ef31ca3
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-04-22 19:06:01 +0530

    circular path and Queues implementation in array

--- src/main/java/com/game/ds/Test.java
+++ src/main/java/com/game/ds/Test.java
@@ -4,20 +4,40 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Test {
-    // Driver code
-    public static void main(String args[]){
-        int[] a = {1,2,3,4,5};
-        rotateArray(a,2);
-        System.out.println(Arrays.stream(a).boxed().collect(Collectors.toList()));
+    public static void main(String[] args){
+        int[] a = {3,1,2,6,4,5,2,1,8}; // output: 3,6,6,6,5,5,8
+        int k = 3;
+        System.out.println("Max sum of subarray: "+getMaxSum(a,k));
     }
-    public static void rotateArray(int[] a, int k){
-        if(k>= a.length)return;
-        for(int i = 0 ;i<k;i++){
-            int temp = a[0];
-            for(int j = 1; j<a.length;j++){
-                a[j-1] = a[j];
+    public  static List<Integer> getMaxSum(int[] a, int k){
+        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int i = 0;
+        //calculate for first window
+        int max = 0;
+        for(i = 0;i<k;i++){
+            if(!queue.isEmpty() && a[queue.peek()]<a[i]){
+                queue.removeAll(queue);
+                queue.add(i);
+            }else{
+                queue.add(i);
             }
-            a[a.length-1] = temp;
         }
+        if(!queue.isEmpty())list.add(a[queue.peek()]);
+        else list.add(0);
+        for(;i<a.length;i++){
+            int s = i - k;
+            if(!queue.isEmpty() && s==queue.peek()){
+                queue.remove();
+            }
+            if(!queue.isEmpty() && a[queue.peek()] < a[i]){
+                queue.removeAll(queue);
+                queue.add(i);
+            }
+            if(!queue.isEmpty()) list.add(a[queue.peek()]);
+            else  list.add(0);
+        }
+        return list;
     }
+
 }
