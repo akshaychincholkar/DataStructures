b633e6b73a1ff0e2df99c908810ef8e7eef9e7ff
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-05-15 09:34:52 +0530

    parking lot manager

--- src/main/java/com/game/java8/lambda/ParkingLot.java
+++ src/main/java/com/game/java8/lambda/ParkingLot.java
@@ -1,5 +1,7 @@
 package com.game.java8.lambda;
 
+import java.util.Date;
+
 public class ParkingLot {
 
     public static void main(String[] args) {
@@ -17,19 +19,41 @@ public class ParkingLot {
         //car
 //        int[] timeFrame = {1,8,24};
 //        int[] fees = {30,60,100};
-        hrs = 13;
-        mins = 5;
+//        hrs = 14;
+//        mins = 59;
 
         // airport bike
-        int[] timeFrame = {1,8,24};
-        int[] fees = {40,60,80};
+        int[] timeFrame = {0,1,8,24};
+        int[] fees = {0,40,60,80};
 
         // airport car
-//        int[] timeFrame = {1,12,24};
+//        int[] timeFrame = {0,12,24};
 //        int[] fees = {60,80,100};
-        hrs = 14;
-        mins = 59;
-        System.out.println("Fare 2: "+calculateFare(hrs,mins,timeFrame,fees,0));
+
+
+        hrs = 36;
+        mins = 12;
+        System.out.println("Fare 2: "+calculateFareAirport(hrs,mins,timeFrame,fees,0));
+
+        Date d1 = new Date("28-May-2022 8:01:00");
+        Date d2 = new Date("31-May-2022 8:00:00");
+        final int MILLI_TO_HOUR = 1000 * 60 * 60;
+        final int MILLI_TO_MINS = 60 * 1000 ;
+        int totalHours = (int) (d2.getTime() - d1.getTime()) / MILLI_TO_HOUR;
+        int totalMins = (int) (d2.getTime() - d1.getTime()) / MILLI_TO_MINS % 60 ;
+        System.out.println("Time difference: "+totalHours+" and "+totalMins+" mins");
+    }
+    public static int  calculateFareAirport(int hrs, int mins,int[] timeframe, int[] fee, int fare){
+        for(int i = timeframe.length-2;i>=0;i--){
+            if(hrs>=timeframe[i] && hrs<timeframe[i+1])return fee[i];
+        }
+        int calc = hrs/24;
+        int result = 0;
+        if(calc>=1){
+            result = calc*fee[fee.length-1];
+            if(hrs%24>=1) result+=fee[fee.length-1];
+        }
+        return result;
     }
     // 8 hours 15 mins
     public static int  calculateFare(int hrs, int mins,int[] timeframe, int[] fee, int fare){
