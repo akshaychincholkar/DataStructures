0cb195010d66973ac9ed2e93d207e244a6641c80
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-05-16 09:04:36 +0530

    update to the book allocation problem

--- src/main/java/com/game/ds/arrays/BookAllocationProblem.java
+++ src/main/java/com/game/ds/arrays/BookAllocationProblem.java
@@ -41,4 +41,36 @@ public class BookAllocationProblem
         }
         return true;
     }
+
+    // ----------------------------------------------
+    // Another way
+    public static int bookAllocation(int[] a, int k) {
+        int sum =0;
+        for(int element: a) sum+=element;
+        return solve(a,k,0,sum);
+    }
+    private static int solve(int[] a,int k,int l,int r){
+        int mid = ( l + r )/2+1;
+        if(l<r){
+            if(isValid(a,k,mid)){
+                return solve(a,k,l+1,mid);
+            }else{
+                return solve(a,k,mid,r);
+            }
+        }
+        return l;
+    }
+    private static boolean isValid(int[] a, int k , int mid){
+        int sum = 0;
+        int parts =1;
+        for(int i = 0 ;i<a.length;i++){
+            sum+=a[i];
+            if(sum>mid){
+                sum = a[i];
+                parts++;
+            }
+            if(parts>k) return false;
+        }
+        return true;
+    }
 }
