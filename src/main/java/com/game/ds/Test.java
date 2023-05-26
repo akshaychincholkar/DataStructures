86c91fd19ce2957c156f6b24d957939233158256
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-05-27 01:57:50 +0530

    book allocation problem

--- src/main/java/com/game/ds/Test.java
+++ src/main/java/com/game/ds/Test.java
@@ -1,31 +1,39 @@
 package com.game.ds;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Test {
-    public static void main(String[] args){
-//        int[] a = {5,1,2,4,3};
-        int[] a = {1,2,3,4,5};
-        for(int n:a)System.out.print(n+" ");
-        sort(a);
-        System.out.println("After sort");
-        for(int n:a)System.out.print(n+" ");
+    public static void main(String[] args) {
+        int[] a = { 10, 20,30,40};
+        int k = 2;
+        System.out.println("books allocation: "+bookAllocation(a,k));
     }
-    public static void sort(int[] a){
-        boolean flag = true;
-        for(int i = 0 ;i<a.length;i++){
-            for(int j = 0; j < a.length - i - 1;j++){
-                if(a[j]>a[j+1]){
-                    int temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
-                    flag = false;
-//                    System.out.println("Om");
-                }
-                if(flag) break;
+
+    public static int bookAllocation(int[] a, int k) {
+        int sum =0;
+        for(int element: a) sum+=element;
+        return solve(a,k,0,sum);
+    }
+    private static int solve(int[] a,int k,int l,int r){
+        int mid = ( l + r )/2+1;
+        if(l<r){
+            if(isValid(a,k,mid)){
+                return solve(a,k,l+1,mid);
+            }else{
+                return solve(a,k,mid,r);
             }
         }
+        return l;
     }
-
+    private static boolean isValid(int[] a, int k , int mid){
+        int sum = 0;
+        int parts =1;
+        for(int i = 0 ;i<a.length;i++){
+            sum+=a[i];
+            if(sum>mid){
+                sum = a[i];
+                parts++;
+            }
+            if(parts>k) return false;
+        }
+        return true;
+    }
 }
