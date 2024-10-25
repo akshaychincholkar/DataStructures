9ed54fa32ddb1a14d13c0cdf0043cb1b039ca2a9
Darshana <akshaychincholkar@gmail.com>
2024-10-25 23:52:32 +0530

    Min max

---
+++ src/main/java/com/game/faang/arrays/MinMax.java
@@ -0,0 +1,24 @@
+package com.game.faang.arrays;
+
+public class MinMax {
+    public static int minimum(int[] a){
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<a.length;i++){
+            if(min > a[i]) min = a[i];
+        }
+        return min;
+    }
+    public static int maximum(int[] a){
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<a.length;i++){
+            if(max<a[i]) max = a[i];
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {5,4,1,3,2};
+        System.out.println("Minimum :"+minimum(a));
+        System.out.println("Maximum :"+maximum(a));
+    }
+}
---
+++ src/main/java/com/game/faang/arrays/ReverseArray.java
@@ -0,0 +1,2 @@
+package com.game.faang.arrays;public class ReverseArray {
+}
