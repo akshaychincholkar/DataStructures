cd07dfc2ac2d1b51f254faa0d7449f784e73fa2e
Darshana <akshaychincholkar@gmail.com>
2024-11-10 10:41:22 +0530

    Doubly linked list operations - 2

--- src/main/java/com/game/maang/linkedlists/MyDoublyLinkedList.java
+++ src/main/java/com/game/maang/linkedlists/MyDoublyLinkedList.java
@@ -29,15 +29,56 @@ public class MyDoublyLinkedList {
         }
         System.out.println();
     }
-    public void createDoublyLinkedList(){
-        Node node1 = new Node(5);
-        Node node2 = new Node(4);
-        Node node3 = new Node(1);
-        Node node4 = new Node(3);
-        Node node5 = new Node(2);
-
-        node1.pptr = head;
+    public boolean search(int value){
+        Node ptr = head;
+        while(ptr!=null){
+            if(ptr.data == value){
+                return true;
+            }
+            ptr = ptr.nptr;
+        }
+        return false;
     }
+    public boolean delete(int value){
+        Node ptr = head,prev = null;
+        while(ptr!=null){
+            if(ptr.data == value){
+                ptr.nptr.pptr = prev;
+                if(prev != null) prev.nptr = ptr.nptr;
+                else head = head.nptr;
+                return true;
+            }
+            prev = ptr;
+            ptr = ptr.nptr;
+        }
+        return false;
+    }
+    public int length(){
+        int i = 0;
+        Node ptr = head;
+        while(ptr!= null){
+            i++;
+            ptr = ptr.nptr;
+        }
+        return i;
+    }
+    public int getElementFromIndex(int index){
+        if( index >= length() || index <0) return -1;
+        int l_index =0;
+        for(Node ptr = head;ptr!=null;ptr=ptr.nptr,l_index++){
+            if(l_index == index) return ptr.data;
+        }
+        return -1;
+    }
+    public void createDoublyLinkedList(){
+        MyDoublyLinkedList doublyLinkedList = new MyDoublyLinkedList();
+        doublyLinkedList.add(5);
+        doublyLinkedList.add(4);
+        doublyLinkedList.add(1);
+        doublyLinkedList.add(3);
+        doublyLinkedList.add(2);
+    }
+
     public static void main(String[] args) {
         MyDoublyLinkedList doublyLinkedList = new MyDoublyLinkedList();
         doublyLinkedList.add(5);
@@ -45,6 +86,17 @@ public class MyDoublyLinkedList {
         doublyLinkedList.add(1);
         doublyLinkedList.add(3);
         doublyLinkedList.add(2);
+        doublyLinkedList.display();
+        System.out.println("Element at index 0: "+doublyLinkedList.getElementFromIndex(0));
+        System.out.println("Element at index 4: "+doublyLinkedList.getElementFromIndex(4));
+        System.out.println("Element at index 2: "+doublyLinkedList.getElementFromIndex(2));
+        System.out.println("Element at index 12: "+doublyLinkedList.getElementFromIndex(12));
+        System.out.println("The value 3 present: "+(doublyLinkedList.search(3)?"Yes":"No"));
+        System.out.println("The value 13 present: "+(doublyLinkedList.search(13)?"Yes":"No"));
+        System.out.println("Value 3 deleted: "+(doublyLinkedList.delete(3)?"Yes":"No"));
+        doublyLinkedList.display();
+        System.out.println("Value 12 deleted: "+(doublyLinkedList.delete(12)?"Yes":"No"));
+        System.out.println("Value 2 deleted: "+(doublyLinkedList.delete(2)?"Yes":"No"));
         doublyLinkedList.display();
     }
 }
