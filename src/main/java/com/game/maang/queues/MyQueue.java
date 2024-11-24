92cb32d37b75f8f73ad0c1b0786b42b612be7b79
akshaychincholkar <akshaychincholkar@gmail.com>
2024-11-25 00:18:19 +0530

    Queue fundamental operations - 2

--- src/main/java/com/game/maang/queues/MyQueue.java
+++ src/main/java/com/game/maang/queues/MyQueue.java
@@ -18,7 +18,48 @@ public class MyQueue {
         }
         queue[rear] = value;
         rear = (rear + 1) % capacity;
+        size++;
+        System.out.println("value added: "+value);
         return true;
     }
+    public boolean remove(){
+        if(size ==0 && front == rear){
+            System.out.println("[UNDERFLOW]: Queue is empty");
+            return false;
+        }
+        System.out.println("Value removed: "+queue[front]);
+        front = (front +1 )%capacity;
+        size--;
+        return true;
+    }
+    public void display(){
+        int pointer=front;
+        for(int i = 0;i<size;i++,pointer=(pointer+1)%capacity){
+            System.out.print("|"+queue[pointer]+"|");
+        }
+        System.out.println();
+    }
 
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue(8);
+        queue.display();
+        queue.add(3);
+        queue.add(4);
+        queue.display();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.add(8);
+        queue.add(7);
+        queue.add(6);
+        queue.add(5);
+        queue.add(4);
+        queue.add(3);
+        queue.add(2);
+        queue.add(1);
+        queue.add(0);
+        queue.display();
+        queue.remove();
+        queue.display();
+    }
 }
