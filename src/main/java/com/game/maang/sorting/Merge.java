7f861d55fc7c845b44ddd7ba24c3be745bd8bae1
Darshana <akshaychincholkar@gmail.com>
2024-12-03 23:48:59 +0530

    merge sort

---
+++ src/main/java/com/game/maang/sorting/Merge.java
@@ -0,0 +1,17 @@
+package com.game.maang.sorting;
+
+public class Merge {
+    public static void sort(int[] a){
+
+    }
+    public static void display(int[] a){
+        for(int element: a) System.out.print(element+"|");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] a={5,4,1,3,2};
+        display(a);
+        sort(a);
+        display(a);
+    }
+}
