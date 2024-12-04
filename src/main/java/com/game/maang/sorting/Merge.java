b5d563823e095a41cd28d25ec3260597381bf79e
Darshana <akshaychincholkar@gmail.com>
2024-12-04 22:04:26 +0530

    merge sort - 2

--- src/main/java/com/game/maang/sorting/Merge.java
+++ src/main/java/com/game/maang/sorting/Merge.java
@@ -4,6 +4,13 @@ public class Merge {
     public static void sort(int[] a){
 
     }
+    public static void merge(int[] a,int i,int j){
+        int n = a.length;
+        int n1 = n - i -1;
+        int n2 = j-i;
+
+
+    }
     public static void display(int[] a){
         for(int element: a) System.out.print(element+"|");
         System.out.println();
